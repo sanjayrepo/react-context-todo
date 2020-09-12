b88bd858034865b4e436bd7b04fafe5c0e2ff62c
sanjay_pradhan <sanjay.p@nooor.in>
2020-09-12 19:00:43 +0530

    input form added

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -5,11 +5,28 @@ import { TodoContext } from "../context/TodoContext";
 import { ADD_TODO } from "../context/action.types";
 
 const TodoForm = () => {
+    const [todoString, setTodoString] = useState("")
+    const { dispatch } = useContext(TodoContext);
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (todoString === "") {
+            return alert("Please enter a todo")
+        }
+        const todo = {
+            todoString,
+            id: v4()
+        }
+        dispatch({
+            type: ADD_TODO,
+            payload: todo
+        })
+        setTodoString("")
+    }
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <FormGroup>
                 <InputGroup>
-                    <Input type="text" name="todo" id="todo" placeholder="Your next Todo" />
+                    <Input type="text" name="todo" id="todo" placeholder="Your next Todo" value={todoString} onChange={e => setTodoString(e.target.value)} />
                     <InputGroupAddon addonType="prepend">
                         <Button color="warning">Add</Button>
                     </InputGroupAddon>
